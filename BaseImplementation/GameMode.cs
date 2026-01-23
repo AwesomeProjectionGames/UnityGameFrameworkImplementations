52136b40304238d79eb2bab83febc9f96698165f
eliott <eliott.jaquier@gmail.com>
2026-01-23 17:30:46 +0100

    Move GameMode to GameFrameworkUnityImplementation

---
+++ BaseImplementation/GameMode.cs
@@ -0,0 +1,10 @@
+﻿using GameFramework;
+
+namespace UnityGameFrameworkImplementations.BaseImplementation
+{
+    public static class GameMode
+    {
+        public static IGameMode? Current { get; set; }
+        //TODO: Change for game instance
+    }
+}
