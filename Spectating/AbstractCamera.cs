cb7ac2cba181777f5688ac7825a067d731bf10af
eliott <eliott.jaquier@gmail.com>
2026-02-09 02:05:01 +0100

    Abstract camera now have IRaycastable capabilities

--- Spectating/AbstractCamera.cs
+++ Spectating/AbstractCamera.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using GameFramework.Interaction;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace GameFramework.Spectating
@@ -7,7 +8,7 @@ namespace GameFramework.Spectating
     /// A base implementation of the ICamera interface using MonoBehaviour.
     /// More friendly to inspector (that cannot serialize interfaces).
     /// </summary>
-    public abstract class AbstractCamera : MonoBehaviour, ICamera
+    public abstract class AbstractCamera : MonoBehaviour, ICamera, IRaycastable
     {
         public abstract bool IsActive { get; set; }
         public abstract float FieldOfView { get; set; }
@@ -17,5 +18,16 @@ namespace GameFramework.Spectating
         
         [Tooltip("Invoked when the state of the camera changed.")]
         public UnityEvent<bool> OnActiveStateChanged { get; } = new UnityEvent<bool>();
+
+        public RaycastHit? Raycast(LayerMask mask, float maxDistance)
+        {
+            bool hasHit = Raycast(mask, maxDistance, out var hit);
+            return hasHit ? hit : null;
+        }
+
+        public bool Raycast(LayerMask mask, float maxDistance, out RaycastHit hit)
+        {
+            return Physics.Raycast(Transform.position, Transform.forward, out hit, maxDistance, mask);
+        }
     }
 }
